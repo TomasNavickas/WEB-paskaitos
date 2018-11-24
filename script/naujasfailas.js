8acc4e21056115b3618f047a5e6577cf6ac1af11
tomas navickas <tomasnavickas2010@gmail.com>
2018-11-25 00:32:07 +0200

    sudeti failai is destytojo githubo

--- script/naujasfailas.js
+++ script/naujasfailas.js
@@ -12,4 +12,121 @@ while (true) {
     el.color += 1;
 }
 
+var state = false;
+
+var foto = document.getElementById('lempute');
+
+function lempute() {
+    if (state) {
+        foto.src = "img/logo/logo.png";
+    } else {
+        foto.src = "img/logo/kaunas-02.png";
+    }
+
+    state = !state;
+}
+
+$('.lempute').click(funtion(){
+    $(this).src = 'img/logo/kaunas-02.png'
+});
+
+
+
+$(document).ready(function () {
+    $("#lempute").click(function () {
+        $(this).src = "img/logo/logo.png";
+    });
+    $("#lempute").click(function () {
+        $(this).src = "mg/logo/kaunas-02.png";
+    });
+});
+
+
+$(document).ready(function () {
+
+
+    // $(".lempute").click(function () {
+    //     $(this).attr('src', 'pic_bulbon.gif');
+    // });
+
+
+    var state = false;
+
+    $('.lempute').click(function() {
+        if (state) {
+            $(".lempute").click(function () {
+                $(this).attr('src', 'pic_bulbon.gif');
+            });
+        } else {
+            $(".lempute").click(function () {
+                $(this).attr('src', 'pic_bulbff.gif');
+            });
+        }
+
+        state = !state;
+    });
+
+});
+
+
+
+
+
+
+
+$(document).ready(function() {
+
+    $('.cont').click(funtion(){
+
+    });
+
+
+});
+
+
+
+$(function () {
+
+    $('.item').hide();
+
+    $('.pagination').click(function () {
+        $('.item').show();
+    });
+
+    $('.light_bulb').click(function () {
+        $(this).attr('src', 'pic_bulbon.gif');
+    });
+
+    var state = false;
+
+    function lempute() {
+        if (state) {
+            $('.lempute').click(function () {
+                $(this).attr('src', 'pic_bulbon.gif');
+            });
+        } else {
+            $('.lempute').click(function () {
+                $(this).attr('src', 'pic_bulbon.gif');
+            });
+        }
+
+        state = !state;
+    }
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- script/portfolio.js
+++ script/portfolio.js
@@ -78,3 +78,45 @@ function isvestis(ka, ikur) {
 }
 
 isvestis(grupe, "grupe");
+
+
+let klase1 = [
+    {vardas: "Jonas", pavarde: "Jonaitis", amzius: 34},
+    {vardas: "Petras", pavarde: "Petraitis", amzius: 56},
+    {vardas: "Zigmas", pavarde: "Zigmaitis", amzius: 12},
+    {vardas: "Ona", pavarde: "Onaityte", amzius: 45},
+    {vardas: "Zita", pavarde: "Zitiene", amzius: 65},
+    {vardas: "Rita", pavarde: "Rutele", amzius: 21}
+];
+
+let klase2 = [
+    {vardas: "Jonas", pavarde: "ppp", amzius: 12},
+    {vardas: "Petras", pavarde: "ppp", amzius: 12},
+    {vardas: "Zigmas", pavarde: "ppp", amzius: 12},
+    {vardas: "Ona", pavarde: "ppp", amzius: 12},
+    {vardas: "Zita", pavarde: "ppp", amzius: 12},
+    {vardas: "Rita", pavarde: "ppp", amzius: 12}
+];
+
+let spausdinti = function (kur, ka) {
+    kur.innerHTML += ka;
+};
+
+function formuotiSarasa(ka) {
+    let sarasas = '';
+    for (let i = 0; i < ka.length; i++) {
+        let asmuo = ka[i];
+        sarasas += asmuo.vardas + " " + asmuo.pavarde + " amzius: " + asmuo.amzius + "<br/>";
+    }
+
+    return sarasas;
+}
+
+let sarasas1 = formuotiSarasa(klase1);
+let sarasas2 = formuotiSarasa(klase2);
+
+let kur1 = document.getElementsByClassName('testas')[0];
+let kur2 = document.getElementById('testas');
+
+spausdinti(kur1, sarasas1);
+spausdinti(kur2, sarasas2);
